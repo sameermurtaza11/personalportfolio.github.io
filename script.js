70664b91bace0a67b1fe900947a49543ea0a4655
sameermurtaza11 <sameermurtaza11@gmail.com>
2022-08-25 23:11:14 +0500

    Updated on 25/08/2022(11:12PM)

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing", {
+    strings: ["Student", "Web Developer", "Freelancer", "Electrical Engineer"],
+    typeSpeed: 130,
+    backSpeed: 80,
+    loop: true
+}); 
+function DisplayNumber(){
+    alert("WhatsApp Number\n+923135445595");
+}
+function ComingSoon(){
+    alert("Instagram Profile is Avalible Soon");
+}
